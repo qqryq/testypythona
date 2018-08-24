be41bd1f62b143d6d90aebdf1fadcb7cc1cedbc8
Tymoteusz <Tymoteusz@ocir.local>
2018-08-24 23:43:15 +0200

    Zadnie 1.4.4 zabawy z liczbami - reszta zakomentowana

--- 144.py
+++ 144.py
@@ -7,6 +7,8 @@ ile = int(input('Ile liczb wylosować?'))
 lista= [] # pusta lista
 for i in range(0, ile):
     lista.append(randint(0, 100))
+
+'''
 print(lista)
 
 print('Dodwanie elementów na końculisty')
@@ -18,6 +20,7 @@ print(lista)
 print('Zawartość listy ([indeks] wartość):')
 for i, v in enumerate(lista):
     print('[', i, '] ', v)
+print()
 
 print('Elementy w odwróconym porządku')
 for e in reversed(lista):
@@ -29,39 +32,58 @@ for e in sorted(lista):
     print(e, end = " ")
 print()
 
-e = int(input("którą liczbęusunąć?"))
+e = int(input("którą liczbę usunąć?"))
 lista.remove(e)
-print(lista)
+#print(lista)
 
 print('Wstawianie elementów do listy')
 a, i = eval(input('Podaj element i indeks oddzielone przecinkiem: '))
 lista.insert(i, a)
-print(lista)
+#print(lista)
+'''
+
 
 print('Wyszukiwanie i zliczanie elementów w liście')
 e = int(input('Podaj szukaną liczbę: '))
 print('Liczba wystąpięń: ')
 print(lista.count(e))
-print('Indeks pierszego wystąpienia: ')
-if lista.count(e):
-    print(lista.index(e))
-else:
-    print('Brak elementu w liście.')
+print('Liczba wystąpięń liczby "10": ', lista.count(10))
+print('Liczba wystąpięń liczby "6": ', lista.count(6))
+print('Liczba wystąpięń liczby "16": ', lista.count(16))
+print('Liczba wystąpięń liczby "2": ', lista.count(2))
+print('Liczba wystąpięń liczby "28": ', lista.count(28))
+print('Liczba wystąpięń liczby "4": ', lista.count(4))
+print('Liczba wystąpięń liczby "1": ', lista.count(1))
+print('Liczba wystąpięń liczby "9": ', lista.count(9))
+print('Liczba wystąpięń liczby "30": ', lista.count(30))
+print('Liczba wystąpięń liczby "8": ', lista.count(8))
 
+
+#print('Indeks pierszego wystąpienia: ')
+#if lista.count(e):
+#    print(lista.index(e))
+#else:
+#    print('Brak elementu w liście.')
+
+
+'''
 print('Pobieramy i usuwam ostatni element z listy: ')
 print(lista.pop())
-print(lista)
+#print(lista)
+'''
 
 print ('Część listy (notacja wycinkowa):')
-i, j = eval(input('Podaj index początkowy i końcowyoddzielone przecinkiem: '))
+i, j = eval(input('Podaj index początkowy i końcowy oddzielone przecinkiem: '))
 print(lista[i:j])
 
+
+'''
 print()
 print('Tupla to lista niemodyfikowalna.')
 print('Prób zmiany tupli eneruje błąd:')
 tupla = tuple(lista)
-tupla[0] = 100
-
+#tupla[0] = 100
+'''
 
 
 
